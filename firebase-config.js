53a867a42939b9139ee249d9e94ac773322e08e4
KAVIAHKA IROSHAN <kavishkairoshan54@gmail.com>
2026-08-13 06:19:14 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 // Copy this file's values from Firebase Console > Project settings > Your apps.
 // Never place Firebase Admin SDK credentials or service-account JSON in browser code.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC3ZKnpBnXZlGihzeO3_SQgNt-1d80bI1o",
+  authDomain: "photo-6ecf2.firebaseapp.com",
+  projectId: "photo-6ecf2",
+  storageBucket: "photo-6ecf2.firebasestorage.app",
+  messagingSenderId: "467388659086",
+  appId: "1:467388659086:web:43eb2fe0050b26adcce0df",
+  measurementId: "G-92DMR9LX63"
 };
